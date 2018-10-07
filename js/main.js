9d21cadab3e2d7a0849d4816f616414e5e498d0f
Unknown <allocompte@hotmail.fr>
2018-10-07 05:13:36 +0200

    Add nav builder

---
+++ js/main.js
@@ -0,0 +1,43 @@
+window.addEventListener('load', function(e) {
+    let nav = document.getElementById('nav');
+    let titles = this.document.querySelectorAll('h2');
+
+    titles.forEach(title => {
+        let link = document.createElement('a');
+        link.classList.add('icon-angle-circled-right');
+        if(title.hasAttribute('id'))
+        {
+            link.setAttribute('href', '#' + title.getAttribute('id'));
+        }
+        else
+        {
+            title.setAttribute('href', '#');
+        }
+        link.innerHTML = '<div>' + title.innerText + '</div>';
+        nav.appendChild(link);
+    });
+});
+
+function setCurrentPart()
+{
+    let titles = document.querySelectorAll('h2');
+    document.querySelectorAll('#nav a').forEach(e => { e.classList.remove('current'); });
+
+    for(var i = titles.length - 1; i >= 0; i--)
+    {
+        console.log(titles[i].innerText + ' ' + titles[i].offsetTop + "/" + window.scrollY);
+        if(titles[i].offsetTop <= window.scrollY)
+        {
+            console.log('Found');
+            let el = document.querySelector('#nav a[href="#' + titles[i].getAttribute('id') + '"]');
+            if(el != null)
+            {
+                console.log('Found the el');
+                el.classList.add('current');
+            }
+            break;
+        }
+    }
+}
+
+window.addEventListener('scroll', setCurrentPart);
