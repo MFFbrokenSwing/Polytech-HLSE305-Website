de5c912ee5353e7870c4b394f0eacf4a7bc20b1c
Unknown <allocompte@hotmail.fr>
2018-12-20 11:10:15 +0100

    Remove bat file

--- compile_less.bat
+++
@@ -1,6 +0,0 @@
-rem TODO : Iterate on all files
-lessc less/core.less css/core.css
-lessc less/index.less css/index.css
-lessc less/menu.less css/menu.css
-lessc less/questions.less css/questions.css
-pause
